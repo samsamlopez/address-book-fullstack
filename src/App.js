b51de04497ad39b3473d07e80ac7ea3d46955ef1
Samuel A. Lopez <samuel.lopez@boom.camp>
2019-07-29 11:44:13 +0800

    Install dependencies

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import './App.css';
+import SignIn from "./component/signIn"
+
+class App extends React.Component{
+  render() {
+    return (
+      <React.Fragment>
+        <SignIn />
+      </React.Fragment>
+    );
+  }
+}
+
+export default App;
---
+++ src/routes.js
@@ -0,0 +1,5 @@
+import React from 'react';
+import { Route, Switch } from 'react-router-dom';
+
+import SignIn from "./component/signIn";
+import SignUp from "./component/signUp";
